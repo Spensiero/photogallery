c0bbd46e6ba43512bea07260a96908a24474089f
Spensiero <diegospensiero@hotmail.it>
2021-05-10 22:07:37 +0200

    Detail

--- src/components/Detail.tsx
+++ src/components/Detail.tsx
@@ -1,11 +1,10 @@
 import React, { Component } from 'react'
+import { Link } from "react-router-dom"
+import './Detail.scss'
 
 export default class Detail extends Component<any> {
     constructor(props: any){
         super(props);
-    }
-    componentDidMount(){
-        
     }
     render() {
         let dataDetail = this.props.location.search
@@ -17,8 +16,13 @@ export default class Detail extends Component<any> {
         }
         return (
             <div>
-               {dataDetail.name ? dataDetail.name : ''}
-            </div>
+                {dataDetail.url && <img className='imageFullScreen' src={dataDetail.url}/>}
+                <Link to="/"><svg style={{width: '30px', height: '30px', position: 'fixed', top: '20px', right: '20px', fill: "#fff", cursor: "pointer", backgroundColor: "rgba(0,0,0,0.5)"}} focusable="false" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"></path></svg></Link>
+                <div className='imageInfo'>
+                    {dataDetail.title && <div style={{width: "calc(100% - 40px)", whiteSpace: "nowrap",overflow: "hidden", textOverflow: "ellipsis"}}><span className='imageInfoText'>Title: </span>{dataDetail.title}</div>}
+                    {dataDetail.author && <div><span className='imageInfoText'>Author: </span>{dataDetail.author}</div>}
+                </div>
+            </div>    
         )
     }
 }
--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,12 +1,15 @@
 import React, { Component } from 'react'
-import { Link } from "react-router-dom";
+import { Link } from "react-router-dom"
 import axios from "axios"
+import ClipLoader from "react-spinners/ClipLoader"
 import './Gallery.scss';
 
 interface State {
     dataImages: any;
+    dataImagesRecovery: any;
     arrThumbs: any;
     scroll: boolean;
+    noImagesFound: boolean;
 }
 
 export default class Gallery extends Component<any,State> {
@@ -15,34 +18,45 @@ export default class Gallery extends Component<any,State> {
         this.state = {
             arrThumbs: [],
             dataImages: [],
-            scroll: false 
+            dataImagesRecovery: [],
+            scroll: false,
+            noImagesFound: false 
         };
+        this.onMouseWheel = this.onMouseWheel.bind(this)
     }
       
     componentDidMount(){
-        document.addEventListener("mousewheel", ()=>{
-            this.getDataImages()
-            this.setScrollActive()
-        })
+        document.addEventListener("mousewheel", this.onMouseWheel, true)
         this.getFirstDataImages()
     }
 
     componentWillUnmount(){
-        document.addEventListener("mousewheel", ()=>{})
+        document.removeEventListener("mousewheel",this.onMouseWheel, true)
+    }
+
+    onMouseWheel(){
+        this.getDataImages()
+        this.setScrollActive()
+        document.removeEventListener("mousewheel", this.onMouseWheel, true)
     }
 
     getFirstDataImages(){
-        axios.get('https://www.reddit.com/r/Pics/top.json')
-        .then((response) => {
-            this.setState(() => ({
-                dataImages: response.data.data.children
+        const _this = this
+        Promise.all([this.getPictureObjOne(), this.getPictureObjTwo()])
+          .then(function (results) {
+            const first = results[0].data.data.children;
+            const second = results[1].data.data.children;
+            const arrPicturesObj = first.concat(second)
+            _this.setState(() => ({
+              dataImages: arrPicturesObj,
+              dataImagesRecovery: arrPicturesObj
             }),()=>{
-                this.setGalleryContent()
+              _this.setGalleryContent()
             });
-        })
-        .catch((e)=>{
-            console.log(e)
-        })
+          })
+          .catch((e)=>{
+              console.log(e)
+          })
     }
 
     setScrollActive(){
@@ -82,9 +96,10 @@ export default class Gallery extends Component<any,State> {
               const fifth = results[4].data.data.children;
               const arrPicturesObj = first.concat(second,third,fourth,fifth)
               _this.setState(() => ({
-                dataImages: arrPicturesObj
+                dataImages: arrPicturesObj,
+                dataImagesRecovery: arrPicturesObj 
               }),()=>{
-                _this.setGalleryContent()
+                _this.filter(null)  
               });
             })
             .catch((e)=>{
@@ -98,7 +113,7 @@ export default class Gallery extends Component<any,State> {
         for (let item of  this.state.dataImages) {
             key = key + 1
             const data = JSON.stringify(item.data)
-            if(item.data.thumbnail && item.data.thumbnail.search("https") == 0)
+            if(item.data.thumbnail && item.data.thumbnail.search("https") === 0)
             arrPictures.push(<li key={key}><Link to={"/detail?"+ data}><img src={item.data.thumbnail}/></Link></li>) 
         }
        
@@ -108,16 +123,46 @@ export default class Gallery extends Component<any,State> {
     }
 
     getThumbs(){
-        if(this.state.arrThumbs.length > 0)
         return this.state.arrThumbs.map((item: any) => item);
+    }
+
+    filter(e: any){
+        const inputValue = document.getElementById('search')
+        const wordFiltered = e ? e.target : inputValue
+        if(wordFiltered.value !== ''){
+            let filteredDataImages =  this.state.dataImagesRecovery.filter((item: any)=>{
+                return item.data.title.includes(wordFiltered.value); 
+            });
+            this.setState({
+                dataImages: filteredDataImages,
+                noImagesFound: filteredDataImages.length > 0 ? false : true
+            },()=>{
+                this.setGalleryContent()
+            })
+        } else {
+            this.setState({
+                dataImages: this.state.dataImagesRecovery
+            },()=>{
+                this.setGalleryContent()
+            })
+        }
     }
     
     render() {
         return (
             <div style={{height:  this.state.scroll ? '100%' : window.innerHeight, overflowY: this.state.scroll ? 'inherit' : 'hidden'}}>
+                <div>
+                    <input id='search' type='text' placeholder='Search' onChange={(e)=> this.filter(e)}/>
+                    <svg style={{width: '20px', height: '20px', position: 'relative', top: '6px', left: '-33px', fill: "#282c34"}} focusable="false" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                        <path d="M15.5 14h-.79l-.28-.27A6.471 6.471 0 0 0 16 9.5 6.5 6.5 0 1 0 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path>
+                    </svg>
+                </div>
                 <ul>
                     {this.getThumbs()}
                 </ul>
+                {this.state.noImagesFound ? 
+                    <p style={{margin: "50px auto"}}>No images found</p> 
+                :   <ClipLoader css={"margin-top: 30px"} color={"withe"} loading={(this.state.arrThumbs.length > 0) ? false : true} size={40} />}
             </div>
         )
     }
