83a7e1673408d14b904a985e674fc831615863e6
Spensiero <diegospensiero@hotmail.it>
2021-05-11 14:06:57 +0200

    detail

--- src/components/Detail.tsx
+++ src/components/Detail.tsx
@@ -16,7 +16,7 @@ export default class Detail extends Component<any> {
         }
         return (
             <div>
-                {dataDetail.url && <img className='imageFullScreen' src={dataDetail.url}/>}
+                {dataDetail.url && <img className='imageFullScreen' src={dataDetail.url} alt=""/>}
                 <Link to="/"><svg style={{width: '30px', height: '30px', position: 'fixed', top: '20px', right: '20px', fill: "#fff", cursor: "pointer", backgroundColor: "rgba(0,0,0,0.5)"}} focusable="false" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"></path></svg></Link>
                 <div className='imageInfo'>
                     {dataDetail.title && <div style={{width: "calc(100% - 40px)", whiteSpace: "nowrap",overflow: "hidden", textOverflow: "ellipsis"}}><span className='imageInfoText'>Title: </span>{dataDetail.title}</div>}
--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -114,7 +114,7 @@ export default class Gallery extends Component<any,State> {
             key = key + 1
             const data = JSON.stringify(item.data)
             if(item.data.thumbnail && item.data.thumbnail.search("https") === 0)
-            arrPictures.push(<li key={key}><Link to={"/detail?"+ data}><img src={item.data.thumbnail}/></Link></li>) 
+            arrPictures.push(<li key={key}><Link to={"/detail?"+ data}><img src={item.data.thumbnail}  alt="image"/></Link></li>) 
         }
        
         this.setState({
@@ -152,7 +152,7 @@ export default class Gallery extends Component<any,State> {
         return (
             <div style={{height:  this.state.scroll ? '100%' : window.innerHeight, overflowY: this.state.scroll ? 'inherit' : 'hidden'}}>
                 <div>
-                    <input id='search' type='text' placeholder='Search' onChange={(e)=> this.filter(e)}/>
+                    <input role="search" id='search' type='text' placeholder='Search' onChange={(e)=> this.filter(e)}/>
                     <svg style={{width: '20px', height: '20px', position: 'relative', top: '6px', left: '-33px', fill: "#282c34"}} focusable="false" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                         <path d="M15.5 14h-.79l-.28-.27A6.471 6.471 0 0 0 16 9.5 6.5 6.5 0 1 0 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path>
                     </svg>
